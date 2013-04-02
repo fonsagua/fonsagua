79e84aa5b31a7e6e4a56f88639247b349025c619
psanxiao <psanxiao@icarto.es>
2013-04-02 18:05:17 +0200

    Added captaciones form

---
+++ src/es/udc/cartolab/gvsig/fonsagua/forms/abastecimiento/CaptacionesForm.java
@@ -0,0 +1,28 @@
+package es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento;
+
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+
+import es.icarto.gvisg.navtableforms.BasicAbstractForm;
+
+@SuppressWarnings("serial")
+public class CaptacionesForm extends BasicAbstractForm {
+
+    public static final String NAME = "captaciones";
+
+    public CaptacionesForm(FLyrVect layer) {
+	super(layer);
+	viewInfo.setTitle("Captaciones");
+    }
+
+    @Override
+    protected String getBasicName() {
+	return NAME;
+    }
+
+    @Override
+    protected void fillSpecificValues() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
