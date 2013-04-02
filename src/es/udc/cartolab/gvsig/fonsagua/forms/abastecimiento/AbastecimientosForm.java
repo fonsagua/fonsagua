68f997c948f6777297595e4b8ea67b7c2eb118ef
psanxiao <psanxiao@icarto.es>
2013-04-02 18:41:10 +0200

    Added alphanumeric tables to abastecimientos form

--- src/es/udc/cartolab/gvsig/fonsagua/forms/abastecimiento/AbastecimientosForm.java
+++ src/es/udc/cartolab/gvsig/fonsagua/forms/abastecimiento/AbastecimientosForm.java
@@ -11,6 +11,9 @@ public class AbastecimientosForm extends BasicAbstractForm {
 
     public static final String NAME = "abastecimientos";
     private TableHandler juntasAguaHandler;
+    private TableHandler coberturaHandler;
+    private TableHandler gestionComercialHandler;
+    private TableHandler gestionFinancieraHandler;
 
     public AbastecimientosForm(FLyrVect layer) {
 	super(layer);
@@ -18,11 +21,26 @@ public class AbastecimientosForm extends BasicAbstractForm {
 	juntasAguaHandler = new TableHandler(JuntasAguaForm.NAME,
 		getWidgetComponents(), "cod_abastecimiento",
 		JuntasAguaForm.colNames, JuntasAguaForm.colAlias);
+	coberturaHandler = new TableHandler(CoberturaForm.NAME,
+		getWidgetComponents(), "cod_abastecimiento",
+		CoberturaForm.colNames, CoberturaForm.colAlias);
+	gestionComercialHandler = new TableHandler(GestionComercialForm.NAME,
+		getWidgetComponents(), "cod_abastecimiento",
+		GestionComercialForm.colNames, GestionComercialForm.colAlias);
+	gestionFinancieraHandler = new TableHandler(GestionFinancieraForm.NAME,
+		getWidgetComponents(), "cod_abastecimiento",
+		GestionFinancieraForm.colNames, GestionFinancieraForm.colAlias);
     }
 
     @Override
     protected void fillSpecificValues() {
 	juntasAguaHandler.fillValues(getFormController().getValue(
+		"cod_abastecimiento"));
+	coberturaHandler.fillValues(getFormController().getValue(
+		"cod_abastecimiento"));
+	gestionComercialHandler.fillValues(getFormController().getValue(
+		"cod_abastecimiento"));
+	gestionFinancieraHandler.fillValues(getFormController().getValue(
 		"cod_abastecimiento"));
     }
 
@@ -30,18 +48,30 @@ public class AbastecimientosForm extends BasicAbstractForm {
     protected void setListeners() {
 	super.setListeners();
 	juntasAguaHandler.reload(new JuntasAguaForm());
+	coberturaHandler.reload(new CoberturaForm());
+	gestionComercialHandler.reload(new GestionComercialForm());
+	gestionFinancieraHandler.reload(new GestionFinancieraForm());
     }
 
     @Override
     protected void removeListeners() {
 	super.removeListeners();
 	juntasAguaHandler.removeListeners();
+	coberturaHandler.removeListeners();
+	gestionComercialHandler.removeListeners();
+	gestionFinancieraHandler.removeListeners();
     }
 
     @Override
     public void onPositionChange(PositionEvent e) {
 	super.onPositionChange(e);
 	juntasAguaHandler.fillValues(getFormController().getValue(
+		"cod_abastecimiento"));
+	coberturaHandler.fillValues(getFormController().getValue(
+		"cod_abastecimiento"));
+	gestionComercialHandler.fillValues(getFormController().getValue(
+		"cod_abastecimiento"));
+	gestionFinancieraHandler.fillValues(getFormController().getValue(
 		"cod_abastecimiento"));
     }
 
