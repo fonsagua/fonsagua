76b0c94440cfae98f28408d32fc694ee43e59cf6
Francisco Puga <fpuga@cartolab.es>
2013-04-05 20:16:30 +0200

    Open forms automatically after digitizing a geometry
    The EndGeometryListener of opencadtools is used to detect when the
    user creates a new geometry, and launch the associated form if
    there is anyone
    
    A button to control the opening of the form is added to the toolbar,
    and the state of the user preference is persisted by means of a
    'lock file' in the gvsig config folder

---
+++ src/es/udc/cartolab/gvsig/fonsagua/AutoFormsExtension.java
@@ -0,0 +1,135 @@
+package es.udc.cartolab.gvsig.fonsagua;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import com.iver.andami.Launcher;
+import com.iver.andami.PluginServices;
+import com.iver.andami.messages.NotificationManager;
+import com.iver.andami.plugins.Extension;
+import com.iver.andami.ui.mdiFrame.JToolBarToggleButton;
+import com.iver.andami.ui.mdiFrame.MDIFrame;
+import com.iver.cit.gvsig.fmap.layers.FLayer;
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+import com.iver.cit.gvsig.listeners.CADListenerManager;
+import com.iver.cit.gvsig.listeners.EndGeometryListener;
+
+import es.icarto.gvsig.navtableforms.BasicAbstractForm;
+import es.icarto.gvsig.navtableforms.gui.tables.TableFormFactory;
+import es.udc.cartolab.gvsig.fonsagua.forms.factories.FonsaguaTableFormFactory;
+
+public class AutoFormsExtension extends Extension {
+
+    private static String KEY_NAME;
+    private boolean formsEnabled = false;
+    private File fileFlag;
+    private final TableFormFactory factory = FonsaguaTableFormFactory
+	    .getInstance();
+
+    @Override
+    public void initialize() {
+	KEY_NAME = getClass().getName();
+	fileFlag = new File(Launcher.getAppHomeDir() + "fonsagua-auto-forms");
+	formsEnabled = getLastSessionEnabilityState();
+
+	if (formsEnabled) {
+	    NTEndGeometryListener listener = new NTEndGeometryListener();
+	    CADListenerManager.addEndGeometryListener(KEY_NAME, listener);
+	}
+
+	registerIcon();
+    }
+
+    private boolean getLastSessionEnabilityState() {
+	return fileFlag.exists();
+    }
+
+    private void registerIcon() {
+	URL icon = this.getClass().getClassLoader()
+		.getResource("images/auto-forms.png");
+	PluginServices.getIconTheme().registerDefault("auto-forms", icon);
+    }
+
+    @Override
+    public void postInitialize() {
+	toggleButton(formsEnabled);
+    }
+
+    @Override
+    public void execute(String actionCommand) {
+
+	if (!formsEnabled) {
+	    NTEndGeometryListener listener = new NTEndGeometryListener();
+	    CADListenerManager.addEndGeometryListener(KEY_NAME, listener);
+	    NotificationManager.addInfo("Formularios automáticos activados");
+	    formsEnabled = true;
+	} else {
+	    CADListenerManager.removeEndGeometryListener(KEY_NAME);
+	    NotificationManager.addInfo("Formularios automáticos desactivados");
+	    formsEnabled = false;
+	}
+
+	toggleButton(formsEnabled);
+	persistsEnabilityState();
+
+    }
+
+    public void persistsEnabilityState() {
+	try {
+	    if (formsEnabled) {
+		fileFlag.createNewFile();
+
+	    } else {
+		fileFlag.delete();
+	    }
+	} catch (IOException e) {
+	}
+    }
+
+    private void toggleButton(boolean pushed) {
+	String tooltip;
+
+	MDIFrame f = ((MDIFrame) PluginServices.getMainFrame());
+
+	if (f.getSelectedTools() == null) {
+	    f.setSelectedTools(f.getInitialSelectedTools());
+	}
+
+	if (!pushed) {
+	    f.setSelectedTool("fonsagua", "empty");
+	    tooltip = PluginServices.getText(this, "enable_auto_forms");
+	} else {
+	    f.setSelectedTool("fonsagua", "auto-forms");
+	    tooltip = PluginServices.getText(this, "disable_auto_forms");
+	}
+	JToolBarToggleButton throwNTButton = (JToolBarToggleButton) f
+		.getComponentByName("auto-forms");
+	if (throwNTButton != null) {
+	    throwNTButton.setToolTip(tooltip);
+	}
+    }
+
+    @Override
+    public boolean isEnabled() {
+	return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return true;
+    }
+
+    private class NTEndGeometryListener implements EndGeometryListener {
+
+	@Override
+	public void endGeometry(FLayer layer, String cadToolKey) {
+	    if (layer instanceof FLyrVect) {
+		BasicAbstractForm form = factory.createForm((FLyrVect) layer);
+		if (form.init()) {
+		    PluginServices.getMDIManager().addWindow(form);
+		}
+	    }
+	}
+    }
+}
---
+++ src/es/udc/cartolab/gvsig/fonsagua/EmptyExtension.java
@@ -0,0 +1,27 @@
+package es.udc.cartolab.gvsig.fonsagua;
+
+import com.iver.andami.plugins.Extension;
+
+public class EmptyExtension extends Extension {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute(String actionCommand) {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+	return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return false;
+    }
+
+}
