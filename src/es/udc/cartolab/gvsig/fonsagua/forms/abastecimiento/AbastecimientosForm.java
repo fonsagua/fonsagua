cbc6c7ae1ff6203686ca97cd34b94064a912d0d6
psanxiao <psanxiao@icarto.es>
2013-04-08 19:07:56 +0200

    Added relationships abastecimientos-comunidades & abastecimientos-fuentes

--- src/es/udc/cartolab/gvsig/fonsagua/forms/abastecimiento/AbastecimientosForm.java
+++ src/es/udc/cartolab/gvsig/fonsagua/forms/abastecimiento/AbastecimientosForm.java
@@ -1,12 +1,17 @@
 package es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento;
 
+import javax.swing.JTextField;
+
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 
 import es.icarto.gvsig.navtableforms.BasicAbstractForm;
 import es.icarto.gvsig.navtableforms.gui.tables.TableHandler;
 import es.icarto.gvsig.navtableforms.gui.tables.VectorialTableHandler;
 import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.ComunidadesForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.factories.FonsaguaTableFormFactory;
+import es.udc.cartolab.gvsig.fonsagua.forms.fuentes.FuentesForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.relationship.TableRelationship;
 import es.udc.cartolab.gvsig.navtable.listeners.PositionEvent;
 
 @SuppressWarnings("serial")
@@ -18,11 +23,15 @@ public class AbastecimientosForm extends BasicAbstractForm {
     private TableHandler gestionComercialHandler;
     private TableHandler gestionFinancieraHandler;
     private TableHandler evaluacionHandler;
+
     private VectorialTableHandler captacionesHandler;
     private VectorialTableHandler depIntermediosHandler;
     private VectorialTableHandler depDistribucionHandler;
     private VectorialTableHandler tuberiasHandler;
     private VectorialTableHandler bombeosHandler;
+
+    private TableRelationship comunidadesRelationship;
+    private TableRelationship fuentesRelationship;
 
     public AbastecimientosForm(FLyrVect layer) {
 	super(layer);
@@ -43,6 +52,7 @@ public class AbastecimientosForm extends BasicAbstractForm {
 	evaluacionHandler = new TableHandler(EvaluacionForm.NAME,
 		getWidgetComponents(), "cod_abastecimiento",
 		EvaluacionForm.colNames, EvaluacionForm.colAlias);
+
 	captacionesHandler = new VectorialTableHandler(
 		toc.getLayerByName(CaptacionesForm.NAME),
 		getWidgetComponents(), "cod_abastecimiento",
@@ -63,6 +73,13 @@ public class AbastecimientosForm extends BasicAbstractForm {
 		toc.getLayerByName(BombeosForm.NAME), getWidgetComponents(),
 		"cod_abastecimiento", BombeosForm.colNames,
 		BombeosForm.colAlias);
+
+	comunidadesRelationship = new TableRelationship(getWidgetComponents(),
+		NAME, "cod_abastecimiento", ComunidadesForm.NAME,
+		"cod_comunidad", "r_abastecimientos_comunidades");
+	fuentesRelationship = new TableRelationship(getWidgetComponents(),
+		NAME, "cod_abastecimiento", FuentesForm.NAME, "cod_fuente",
+		"r_abastecimientos_fuentes");
     }
 
     @Override
@@ -79,6 +96,13 @@ public class AbastecimientosForm extends BasicAbstractForm {
 	depDistribucionHandler.fillValues(codAbastecimiento);
 	tuberiasHandler.fillValues(codAbastecimiento);
 	bombeosHandler.fillValues(codAbastecimiento);
+
+	comunidadesRelationship
+		.setPrimaryPKValue(((JTextField) getWidgetComponents().get(
+			comunidadesRelationship.getPrimaryPKName())).getText());
+	fuentesRelationship
+		.setPrimaryPKValue(((JTextField) getWidgetComponents().get(
+			fuentesRelationship.getPrimaryPKName())).getText());
     }
 
     @Override
@@ -89,6 +113,7 @@ public class AbastecimientosForm extends BasicAbstractForm {
 	gestionComercialHandler.reload(new GestionComercialForm());
 	gestionFinancieraHandler.reload(new GestionFinancieraForm());
 	evaluacionHandler.reload(new EvaluacionForm());
+
 	captacionesHandler.reload(CaptacionesForm.NAME,
 		FonsaguaTableFormFactory.getInstance());
 	depIntermediosHandler.reload(DepIntermediosForm.NAME,
@@ -99,6 +124,10 @@ public class AbastecimientosForm extends BasicAbstractForm {
 		FonsaguaTableFormFactory.getInstance());
 	bombeosHandler.reload(BombeosForm.NAME,
 		FonsaguaTableFormFactory.getInstance());
+
+	comunidadesRelationship.reload();
+	fuentesRelationship.reload();
+
     }
 
     @Override
@@ -109,11 +138,16 @@ public class AbastecimientosForm extends BasicAbstractForm {
 	gestionComercialHandler.removeListeners();
 	gestionFinancieraHandler.removeListeners();
 	evaluacionHandler.removeListeners();
+
 	captacionesHandler.removeListeners();
 	depIntermediosHandler.removeListeners();
 	depDistribucionHandler.removeListeners();
 	tuberiasHandler.removeListeners();
 	bombeosHandler.removeListeners();
+
+	comunidadesRelationship.removeListeners();
+	fuentesRelationship.removeListeners();
+
     }
 
     @Override
