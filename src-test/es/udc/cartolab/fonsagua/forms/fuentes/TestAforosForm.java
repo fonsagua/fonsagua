f11c9cb3810b5106d2f72c1f03747012a42fd391
psanxiao <psanxiao@icarto.es>
2013-04-09 12:15:08 +0200

    Changed schema on fuentes forms tests

--- src-test/es/udc/cartolab/fonsagua/forms/fuentes/TestAforosForm.java
+++ src-test/es/udc/cartolab/fonsagua/forms/fuentes/TestAforosForm.java
@@ -1,13 +1,14 @@
 package es.udc.cartolab.fonsagua.forms.fuentes;
 
 import es.icarto.gvsig.navtableforms.CommonMethodsForTestDBForms;
+import es.udc.cartolab.gvsig.fonsagua.FonsaguaConstants;
 import es.udc.cartolab.gvsig.fonsagua.forms.fuentes.AforosForm;
 
 public class TestAforosForm extends CommonMethodsForTestDBForms {
 
     @Override
     protected String getSchema() {
-	return "public";
+	return FonsaguaConstants.dataSchema;
     }
 
     @Override
