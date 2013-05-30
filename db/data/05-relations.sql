00ca39fc8ce47bc7014b3e0b4757a6d325918530
Jorge López <jlopez@cartolab.es>
2013-05-30 20:43:17 +0200

    Added cascades to the db n-n relations sql.

--- db/data/05-relations.sql
+++ db/data/05-relations.sql
@@ -1,10 +1,10 @@
 CREATE TABLE fonsagua.r_abastecimientos_comunidades (
        cod_abastecimiento VARCHAR
 	       NOT NULL
-	       REFERENCES fonsagua.abastecimientos(cod_abastecimiento),
+	       REFERENCES fonsagua.abastecimientos(cod_abastecimiento) ON UPDATE CASCADE ON DELETE CASCADE,
        cod_comunidad VARCHAR
 	       NOT NULL
-       	       REFERENCES fonsagua.comunidades(cod_comunidad),
+       	       REFERENCES fonsagua.comunidades(cod_comunidad) ON UPDATE CASCADE ON DELETE CASCADE,
        PRIMARY KEY (cod_abastecimiento, cod_comunidad)
 );
 
@@ -14,10 +14,10 @@ ALTER TABLE fonsagua.r_abastecimientos_comunidades OWNER TO fonsagua;
 CREATE TABLE fonsagua.r_abastecimientos_fuentes (
        cod_abastecimiento VARCHAR
 	       NOT NULL
-       	       REFERENCES fonsagua.abastecimientos(cod_abastecimiento),
+       	       REFERENCES fonsagua.abastecimientos(cod_abastecimiento) ON UPDATE CASCADE ON DELETE CASCADE,
        cod_fuente VARCHAR
 	       NOT NULL
-       	       REFERENCES fonsagua.fuentes(cod_fuente),
+       	       REFERENCES fonsagua.fuentes(cod_fuente) ON UPDATE CASCADE ON DELETE CASCADE,
        PRIMARY KEY (cod_abastecimiento, cod_fuente)
 );
 
