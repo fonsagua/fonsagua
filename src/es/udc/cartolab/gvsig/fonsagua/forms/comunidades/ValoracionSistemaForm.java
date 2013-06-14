7e90629e8f29c4316f980f915a7c07da8ddddf58
Jorge López <jlopez@cartolab.es>
2013-06-14 13:36:54 +0200

    Fix #451 Abastecimientos filtered in valoracion sistema.
    Now abastecimientos are filtered by the current
    comunidad in valoracion de sistema form.

--- src/es/udc/cartolab/gvsig/fonsagua/forms/comunidades/ValoracionSistemaForm.java
+++ src/es/udc/cartolab/gvsig/fonsagua/forms/comunidades/ValoracionSistemaForm.java
@@ -1,6 +1,11 @@
 package es.udc.cartolab.gvsig.fonsagua.forms.comunidades;
 
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
 import es.icarto.gvsig.navtableforms.gui.tables.AbstractSubForm;
+import es.icarto.gvsig.navtableforms.ormlite.domainvalidator.listeners.DependentComboboxesHandler;
 
 @SuppressWarnings("serial")
 public class ValoracionSistemaForm extends AbstractSubForm {
@@ -13,6 +18,9 @@ public class ValoracionSistemaForm extends AbstractSubForm {
 "ID Comunidad", "Código Abastecimiento", "Cobros", "Servicio"
     };
 
+    private JComponent codComunidad;
+    private DependentComboboxesHandler abastecimientosDomainHandler;
+
     @Override
     protected String getBasicName() {
 	return NAME;
@@ -20,5 +28,25 @@ public class ValoracionSistemaForm extends AbstractSubForm {
 
     @Override
     protected void fillSpecificValues() {
+	abastecimientosDomainHandler.updateComboBoxValues();
+    }
+
+    @Override
+    public void setListeners() {
+	super.setListeners();
+	codComunidad = getWidgets().get("cod_comunidad");
+	JComboBox codAbastecimiento = (JComboBox) getWidgets().get(
+		"cod_abastecimiento");
+	abastecimientosDomainHandler = new DependentComboboxesHandler(this,
+		codComunidad, codAbastecimiento);
+	((JTextField) codComunidad)
+		.addActionListener(abastecimientosDomainHandler);
+    }
+
+    @Override
+    public void removeListeners() {
+	super.removeListeners();
+	((JTextField) codComunidad)
+		.addActionListener(abastecimientosDomainHandler);
     }
 }
