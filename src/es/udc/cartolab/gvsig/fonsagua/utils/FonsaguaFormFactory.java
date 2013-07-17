bab62eac84e357bf52c1ed5e18e517eb8b654295
Jorge López <jlopez@cartolab.es>
2013-07-17 11:19:04 +0200

    Adde method for obtaining subforms to the factory.

--- src/es/udc/cartolab/gvsig/fonsagua/utils/FonsaguaFormFactory.java
+++ src/es/udc/cartolab/gvsig/fonsagua/utils/FonsaguaFormFactory.java
@@ -24,6 +24,7 @@ import com.iver.cit.gvsig.project.documents.table.gui.Table;
 import com.iver.cit.gvsig.project.documents.view.gui.BaseView;
 
 import es.icarto.gvsig.navtableforms.AbstractForm;
+import es.icarto.gvsig.navtableforms.gui.tables.AbstractSubForm;
 import es.icarto.gvsig.navtableforms.utils.FormFactory;
 import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
 import es.icarto.gvsig.navtableforms.utils.TOCTableManager;
@@ -31,19 +32,42 @@ import es.udc.cartolab.gvsig.fonsagua.FonsaguaConstants;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.AbastecimientosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.BombeosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.CaptacionesForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.CoberturaForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.DepDistribucionForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.DepIntermediosForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.EvaluacionForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.GestComercialForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.GestFinancieraForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.JuntasAguaForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.SingletonAbastecimientosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.abastecimiento.TuberiasForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.AdescosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.AmenazasForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.AreasPotencialesRiegoForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.CapacitacionesRiesgosForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.CargosPublicosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.CentrosEducativosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.CentrosSaludForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.ComunidadesForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.CooperativasForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.DatosConsumoForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.EntrevistadoresForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.EntrevistadosForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.FuentesContaminacionForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.GanaderiaForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.HabitosConsumoForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.ImplicacionComunidadForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.OngsForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.OtrasOrganizacionesForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.OtrosServiciosForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.ProduccionConsumoForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.PuntosViviendasForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.SingletonComunidadesForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.SubcuencasForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.TiposCultivosForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.comunidades.ValoracionSistemaForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.fuentes.AforosForm;
+import es.udc.cartolab.gvsig.fonsagua.forms.fuentes.AnaliticasForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.fuentes.FuentesForm;
 import es.udc.cartolab.gvsig.fonsagua.forms.fuentes.SingletonFuentesForm;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
@@ -118,6 +142,58 @@ public class FonsaguaFormFactory extends FormFactory {
 	    } else if (layer.getName().equals(FuentesContaminacionForm.NAME)) {
 		return new FuentesContaminacionForm(layer);
 	    }
+	}
+	return null;
+    }
+
+    @Override
+    public AbstractSubForm createSubForm(String tableName) {
+	if (tableName.equals(CoberturaForm.NAME)) {
+	    return new CoberturaForm();
+	} else if (tableName.equals(EvaluacionForm.NAME)) {
+	    return new EvaluacionForm();
+	} else if (tableName.equals(GestComercialForm.NAME)) {
+	    return new GestComercialForm();
+	} else if (tableName.equals(GestFinancieraForm.NAME)) {
+	    return new GestFinancieraForm();
+	} else if (tableName.equals(JuntasAguaForm.NAME)) {
+	    return new JuntasAguaForm();
+	} else if (tableName.equals(AdescosForm.NAME)) {
+	    return new AdescosForm();
+	} else if (tableName.equals(CapacitacionesRiesgosForm.NAME)) {
+	    return new CapacitacionesRiesgosForm();
+	} else if (tableName.equals(CargosPublicosForm.NAME)) {
+	    return new CargosPublicosForm();
+	} else if (tableName.equals(CooperativasForm.NAME)) {
+	    return new CooperativasForm();
+	} else if (tableName.equals(DatosConsumoForm.NAME)) {
+	    return new DatosConsumoForm();
+	} else if (tableName.equals(EntrevistadoresForm.NAME)) {
+	    return new EntrevistadoresForm();
+	} else if (tableName.equals(EntrevistadosForm.NAME)) {
+	    return new EntrevistadosForm();
+	} else if (tableName.equals(GanaderiaForm.NAME)) {
+	    return new GanaderiaForm();
+	} else if (tableName.equals(HabitosConsumoForm.NAME)) {
+	    return new HabitosConsumoForm();
+	} else if (tableName.equals(ImplicacionComunidadForm.NAME)) {
+	    return new ImplicacionComunidadForm();
+	} else if (tableName.equals(OngsForm.NAME)) {
+	    return new OngsForm();
+	} else if (tableName.equals(OtrasOrganizacionesForm.NAME)) {
+	    return new OtrasOrganizacionesForm();
+	} else if (tableName.equals(ProduccionConsumoForm.NAME)) {
+	    return new ProduccionConsumoForm();
+	} else if (tableName.equals(SubcuencasForm.NAME)) {
+	    return new SubcuencasForm();
+	} else if (tableName.equals(TiposCultivosForm.NAME)) {
+	    return new TiposCultivosForm();
+	} else if (tableName.equals(ValoracionSistemaForm.NAME)) {
+	    return new ValoracionSistemaForm();
+	} else if (tableName.equals(AforosForm.NAME)) {
+	    return new AforosForm();
+	} else if (tableName.equals(AnaliticasForm.NAME)) {
+	    return new AnaliticasForm();
 	}
 	return null;
     }
