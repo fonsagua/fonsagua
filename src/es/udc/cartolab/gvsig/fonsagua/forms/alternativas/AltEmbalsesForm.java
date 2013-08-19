519c7b08af32f1cd142844ecda97864b093695e3
Francisco Puga <fpuga@cartolab.es>
2013-08-19 13:00:15 +0200

    Add 'Embalses' form

---
+++ src/es/udc/cartolab/gvsig/fonsagua/forms/alternativas/AltEmbalsesForm.java
@@ -0,0 +1,19 @@
+package es.udc.cartolab.gvsig.fonsagua.forms.alternativas;
+
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+
+import es.icarto.gvsig.navtableforms.BasicAbstractForm;
+
+@SuppressWarnings("serial")
+public class AltEmbalsesForm extends BasicAbstractForm {
+
+    public static final String NAME = "alt_embalses";
+    public AltEmbalsesForm(FLyrVect layer) {
+	super(layer);
+    }
+
+    @Override
+    protected String getBasicName() {
+	return NAME;
+    }
+}
