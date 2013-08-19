4c7fbaba5b0b37a91f39986adb48684654f7473e
Francisco Puga <fpuga@cartolab.es>
2013-08-19 12:59:49 +0200

    Add 'Alternativas' form

---
+++ src-test/es/udc/cartolab/gvsig/fonsagua/forms/alternativas/TestAlternativasForm.java
@@ -0,0 +1,18 @@
+package es.udc.cartolab.gvsig.fonsagua.forms.alternativas;
+
+import es.icarto.gvsig.navtableforms.CommonMethodsForTestDBForms;
+import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaConstants;
+
+public class TestAlternativasForm extends CommonMethodsForTestDBForms {
+
+    @Override
+    protected String getSchema() {
+	return FonsaguaConstants.dataSchema;
+    }
+
+    @Override
+    protected String getTableName() {
+	return AlternativasForm.NAME;
+    }
+
+}
