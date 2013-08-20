af8be9bd0ee234cb2d7fa97633c6da99e7c3f670
Francisco Puga <fpuga@cartolab.es>
2013-08-20 12:35:45 +0200

    Triggers for automatic fields of alt_fuentes

---
+++ db/data/11-triggers-alternatives.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION fonsagua.alt_fuentes_compute_field_trigger() RETURNS TRIGGER AS $alt_fuentes_compute_field_trigger$
+       DECLARE
+	coef_q_eco_t REAL;
+       BEGIN
+	SELECT COALESCE(coef_q_eco,0) INTO coef_q_eco_t FROM fonsagua.preferencias WHERE cod_alternativa = NEW.cod_alternativa;
+
+	IF (coef_q_eco_t > 0 AND NEW.aforo > 0) THEN NEW.q_ecologico = coef_q_eco_t * NEW.AFORO; ELSE NEW.q_ecologico = NULL; END IF;
+
+	IF (NEW.q_usar IS NULL OR NEW.q_usar > coef_q_eco_t  OR NEW.q_usar < 0) THEN NEW.q_usar = NEW.q_ecologico; END IF;
+
+	NEW.q_calculo = -NEW.q_usar;
+
+	RETURN NEW;
+       END;
+$alt_fuentes_compute_field_trigger$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS alt_fuentes_compute_field_trigger ON fonsagua.alt_fuentes;
+CREATE TRIGGER alt_fuentes_compute_field_trigger
+BEFORE INSERT OR UPDATE ON fonsagua.alt_fuentes
+FOR EACH ROW EXECUTE PROCEDURE fonsagua.alt_fuentes_compute_field_trigger();
+
+
+
---
+++ db/tests/triggers-alternativas.sql
@@ -0,0 +1,42 @@
+BEGIN;
+
+SELECT PLAN(9);
+
+SELECT trigger_is('fonsagua', 'alt_fuentes', 'alt_fuentes_compute_field_trigger', 'fonsagua', 'alt_fuentes_compute_field_trigger');
+
+INSERT INTO fonsagua.preferencias (cod_alternativa, coef_q_eco) VALUES ('TEST-ALT-FUENTES', 0.5);
+
+INSERT INTO fonsagua.alt_fuentes(cod_alternativa, aforo, q_usar) VALUES ('TEST-ALT-FUENTES', 2, 0);
+SELECT is(q_ecologico, 1::numeric)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_usar, 0::numeric)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_calculo, 0::numeric)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+UPDATE fonsagua.alt_fuentes SET aforo=-5 WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+SELECT is(q_ecologico, NULL)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_usar, 0::numeric)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+INSERT INTO fonsagua.alt_fuentes(cod_alternativa, aforo, q_usar) VALUES ('TEST-ALT-FUENTES', 0.5, NULL);
+SELECT is(q_ecologico, 0.25)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_usar, 0.25)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_calculo, -0.25)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
