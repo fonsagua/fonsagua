0bca0ab90a0b979b751a958a92499b7d83090b9d
Jorge López <jlopez@cartolab.es>
2013-08-20 19:30:30 +0200

    Open alternative dialog improved.
    Now we have comboboxes for selecting the alternative
    by filtering with their area entities.

--- src/es/udc/cartolab/gvsig/fonsagua/OpenAlternativeExtension.java
+++ src/es/udc/cartolab/gvsig/fonsagua/OpenAlternativeExtension.java
@@ -1,6 +1,13 @@
 package es.udc.cartolab.gvsig.fonsagua;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import javax.swing.JOptionPane;
 
 import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
@@ -13,6 +20,7 @@ import com.iver.cit.gvsig.project.documents.ProjectDocumentFactory;
 import com.iver.cit.gvsig.project.documents.view.ProjectViewFactory;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
+import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
 import es.udc.cartolab.gvsig.elle.utils.ELLEMap;
 import es.udc.cartolab.gvsig.elle.utils.LoadLegend;
 import es.udc.cartolab.gvsig.elle.utils.MapDAO;
@@ -36,16 +44,95 @@ public class OpenAlternativeExtension extends Extension {
 
     @Override
     public void execute(String actionCommand) {
-	OpenAlternativeDialog dialog = new OpenAlternativeDialog();
-	OpenAlternativeExtension.code = dialog.getCode();
-	if (OpenAlternativeExtension.code == null) {
-	    // TODO: catch error
-	    return;
+	try {
+	    String[] fields = new String[2];
+	    String[] orderBy = new String[1];
+	    Map<String, Map<String, Map<String, List<String>>>> divsCodes = new HashMap<String, Map<String, Map<String, List<String>>>>();
+	    fields[0] = AlternativasForm.PKFIELD;
+	    fields[1] = AlternativasForm.CANTONFK;
+	    orderBy[0] = AlternativasForm.PKFIELD;
+	    String[][] alternativas = DBSession.getCurrentSession().getTable(
+		    AlternativasForm.NAME, FonsaguaConstants.dataSchema,
+		    fields, "", orderBy, false);
+	    if (alternativas.length < 1) {
+		JOptionPane.showMessageDialog(new TOCLayerManager().getView(),
+			PluginServices.getText(this,
+				"no_alternatives_to_open_msg"),
+			PluginServices.getText(this,
+				"no_alternatives_to_open_title"),
+			JOptionPane.INFORMATION_MESSAGE);
+		return;
+	    }
+
+	    Map<String, String> departNames = new HashMap<String, String>();
+	    fields[0] = FonsaguaConstants.departamentosPK;
+	    fields[1] = FonsaguaConstants.departamentosName;
+	    orderBy[0] = FonsaguaConstants.departamentosPK;
+	    String[][] departamentos = DBSession.getCurrentSession().getTable(
+		    FonsaguaConstants.departamentosTable,
+		    FonsaguaConstants.baseSchema, fields, "", orderBy, false);
+	    for (String[] depart : departamentos) {
+		departNames.put(depart[0], depart[1]);
+	    }
+
+	    Map<String, String> municNames = new HashMap<String, String>();
+	    fields[0] = FonsaguaConstants.municipiosPK;
+	    fields[1] = FonsaguaConstants.municipiosName;
+	    orderBy[0] = FonsaguaConstants.municipiosPK;
+	    String[][] municipios = DBSession.getCurrentSession().getTable(
+		    FonsaguaConstants.municipiosTable,
+		    FonsaguaConstants.baseSchema, fields, "", orderBy, false);
+	    String departCode;
+	    for (String[] munic : municipios) {
+		if ((munic[0] != null) && (munic[0].length() >= 2)) {
+		    municNames.put(munic[0], munic[1]);
+		}
+
+	    }
+
+	    Map<String, String> cantonNames = new HashMap<String, String>();
+	    fields[0] = FonsaguaConstants.cantonesPK;
+	    fields[1] = FonsaguaConstants.cantonesName;
+	    orderBy[0] = FonsaguaConstants.cantonesPK;
+	    String[][] cantones = DBSession.getCurrentSession().getTable(
+		    FonsaguaConstants.cantonesTable,
+		    FonsaguaConstants.baseSchema, fields, "", orderBy, false);
+	    String municCode;
+	    for (String[] canton : cantones) {
+		if ((canton[0] != null) && (canton[0].length() >= 4)) {
+		    cantonNames.put(canton[0], canton[1]);
+		}
+	    }
+
+	    for (String[] alternativa : alternativas) {
+		if ((alternativa[0] != null) && (alternativa[1] != null)
+			&& (alternativa[1].length() >= 4)) {
+		    departCode = alternativa[1].substring(0, 2);
+		    municCode = alternativa[1].substring(0, 4);
+		    if (!divsCodes.containsKey(departCode)) {
+			divsCodes
+				.put(departCode,
+					new HashMap<String, Map<String, List<String>>>());
+		    }
+		    if (!divsCodes.get(departCode).containsKey(municCode)) {
+			divsCodes.get(departCode).put(municCode,
+				new HashMap<String, List<String>>());
+		    }
+		    if (!divsCodes.get(departCode).get(municCode)
+			    .containsKey(alternativa[1])) {
+			divsCodes.get(departCode).get(municCode)
+				.put(alternativa[1], new ArrayList<String>());
+		    }
+		    divsCodes.get(departCode).get(municCode)
+				.get(alternativa[1]).add(alternativa[0]);
+		}
+	    }
+	    OpenAlternativeDialog dialog = new OpenAlternativeDialog(divsCodes,
+		    departNames, municNames, cantonNames);
+	    PluginServices.getMDIManager().addWindow(dialog);
+	} catch (SQLException e) {
+	    e.printStackTrace();
 	}
-	// TODO: What must be done if we already opened an alternative and the
-	// user press the button again.
-	// PluginServices.getMDIManager().closeAllWindows();
-	openAlternative(code);
     }
 
     public static void openAlternative(String code) {
--- src/es/udc/cartolab/gvsig/fonsagua/alternativas/ui/OpenAlternativeDialog.java
+++ src/es/udc/cartolab/gvsig/fonsagua/alternativas/ui/OpenAlternativeDialog.java
@@ -1,50 +1,247 @@
 package es.udc.cartolab.gvsig.fonsagua.alternativas.ui;
 
-import java.sql.SQLException;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
-import javax.swing.JOptionPane;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.apache.log4j.Logger;
 
 import com.iver.andami.PluginServices;
-import com.iver.andami.ui.mdiFrame.MDIFrame;
+import com.iver.andami.ui.mdiManager.IWindow;
+import com.iver.andami.ui.mdiManager.WindowInfo;
 
-import es.udc.cartolab.gvsig.fonsagua.forms.alternativas.AlternativasForm;
-import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaConstants;
-import es.udc.cartolab.gvsig.users.utils.DBSession;
+import es.udc.cartolab.gvsig.fonsagua.OpenAlternativeExtension;
 
-public class OpenAlternativeDialog {
+public class OpenAlternativeDialog extends JPanel implements IWindow,
+	ActionListener {
 
-    private String code;
+    private static Logger logger = Logger.getLogger("AlternativesExtension");
 
-    public OpenAlternativeDialog() {
-	String tableName = AlternativasForm.NAME;
-	String schema = FonsaguaConstants.dataSchema;
-	String fieldName = FonsaguaConstants.COD_ALTERNATIVA;
+    /*
+     * The next properties are the ones related to the interface itself, so they
+     * don't need a real explanation
+     */
 
-	String[] options;
-	try {
-	    options = DBSession.getCurrentSession().getDistinctValues(
-		    tableName, schema, fieldName);
-	    if (options.length == 0) {
-		JOptionPane.showMessageDialog(
-			(MDIFrame) PluginServices.getMainFrame(),
-			"No hay alternativas en el sistema");
-		code = null;
-		return;
+    private WindowInfo windowInfo = null;
+    private JButton cancelButton = null;
+    private JButton okButton = null;
+    // Here we store the relationships departamento -> municipio -> cantón ->
+    // alternativa
+    private Map<String, Map<String, Map<String, List<String>>>> divsCodes;
+    // Translation from cod_departamento to nombre_departamento
+    private Map<String, String> departNames;
+    // Translation from cod_municipio to nombre_municipio
+    private Map<String, String> municNames;
+    // Translation from cod_canton to nombre_canton
+    private Map<String, String> cantonNames;
+
+    private JComboBox departCombo = null;
+    private JComboBox municCombo = null;
+    private JComboBox cantonCombo = null;
+    private JComboBox alternCombo = null;
+
+    public WindowInfo getWindowInfo() {
+	if (windowInfo == null) {
+	    windowInfo = new WindowInfo(WindowInfo.MODALDIALOG
+		    | WindowInfo.PALETTE | WindowInfo.NOTCLOSABLE);
+	    windowInfo.setTitle(PluginServices.getText(this,
+		    "new_alternative_title"));
+	    Dimension dim = getPreferredSize();
+	    int width, height = 0;
+	    if (dim.getHeight() > 550) {
+		height = 550;
+	    } else {
+		height = 110;
 	    }
-	    code = (String) JOptionPane.showInputDialog(
-		    (MDIFrame) PluginServices.getMainFrame(),
-		    "Escoja alternativa", "", JOptionPane.QUESTION_MESSAGE,
-		    null, options, options[0]);
-	} catch (SQLException e) {
-	    code = null;
-	    throw new RuntimeException(
-		    "Error externo. No se puede abrir la alternativa", e);
+	    if (dim.getWidth() > 550) {
+		width = 550;
+	    } else {
+		width = new Double(dim.getWidth()).intValue() + 5;
+	    }
+	    windowInfo.setWidth(width);
+	    windowInfo.setHeight(height);
 	}
+	return windowInfo;
+    }
+
+    public OpenAlternativeDialog(
+	    Map<String, Map<String, Map<String, List<String>>>> divsCodes,
+	    Map<String, String> departNames, Map<String, String> municNames,
+	    Map<String, String> cantonNames) {
+	super();
+	this.divsCodes = divsCodes;
+	this.departNames = departNames;
+	this.municNames = municNames;
+	this.cantonNames = cantonNames;
+	try {
+	    initialize();
+	} catch (Exception e) {
+	    logger.error(e.getMessage(), e);
+	}
+    }
+
+    private void initialize() throws Exception {
+
+	this.setLayout(new MigLayout());
+
+	JLabel label = new JLabel(PluginServices.getText(this, "Cod_depart"));
+	this.add(label);
+
+	Vector<Item> items = new Vector<Item>();
+	for (String departCod : divsCodes.keySet()) {
+	    items.add(new Item(departCod, departNames.get(departCod)));
+	}
+
+	departCombo = new JComboBox(items);
+	departCombo.addItemListener(new ItemListener() {
+	    public void itemStateChanged(ItemEvent event) {
+		if (event.getStateChange() == ItemEvent.SELECTED) {
+		    updateMunicCombo();
+		    updateCantonCombo();
+		    updateAlternativaCombo();
+		}
+	    }
+	});
+	this.add(departCombo, "w 220!, wrap");
+
+	label = new JLabel(PluginServices.getText(this, "Cod_munic"));
+	this.add(label);
+
+	municCombo = new JComboBox();
+	municCombo.addItemListener(new ItemListener() {
+	    public void itemStateChanged(ItemEvent event) {
+		if (event.getStateChange() == ItemEvent.SELECTED) {
+		    updateCantonCombo();
+		    updateAlternativaCombo();
+		}
+	    }
+	});
+	this.add(municCombo, "w 220!, wrap");
+	updateMunicCombo();
+
+	label = new JLabel(PluginServices.getText(this, "Cod_canton"));
+	this.add(label);
+
+	cantonCombo = new JComboBox();
+	cantonCombo.addItemListener(new ItemListener() {
+	    public void itemStateChanged(ItemEvent event) {
+		if (event.getStateChange() == ItemEvent.SELECTED) {
+		    updateAlternativaCombo();
+		}
+	    }
+	});
+	this.add(cantonCombo, "w 220!, wrap");
+	updateCantonCombo();
+
+	label = new JLabel(PluginServices.getText(this, "Cod_alternativa"));
+	this.add(label);
+
+	alternCombo = new JComboBox();
+	this.add(alternCombo, "w 220!, wrap");
+	updateAlternativaCombo();
+
+	okButton = new JButton();
+	okButton.setText(PluginServices.getText(this, "Ok"));
+	okButton.addActionListener(this);
+
+	cancelButton = new JButton();
+	cancelButton.setText(PluginServices.getText(this, "Cancel"));
+	cancelButton.addActionListener(this);
+
+	this.add(okButton, "right, bottom, cell 1 5");
+	this.add(cancelButton, "right, bottom, cell 1 5");
 
     }
 
-    public String getCode() {
-	return code;
+    private void updateMunicCombo() {
+	Vector<Item> items = new Vector<Item>();
+	String selDepart = ((Item) departCombo.getSelectedItem()).id;
+	if (divsCodes.containsKey(selDepart)) {
+	    for (String municCod : divsCodes.get(selDepart).keySet()) {
+		items.add(new Item(municCod, municNames.get(municCod)));
+	    }
+	}
+	municCombo.setModel(new DefaultComboBoxModel(items));
+	municCombo.setSelectedIndex(0);
+    }
+
+    private void updateCantonCombo() {
+	Vector<Item> items = new Vector<Item>();
+	String selDepart = ((Item) departCombo.getSelectedItem()).id;
+	String selMunic = ((Item) municCombo.getSelectedItem()).id;
+	if ((divsCodes.containsKey(selDepart))
+		&& (divsCodes.get(selDepart).containsKey(selMunic))) {
+	    for (String cantonCod : divsCodes.get(selDepart).get(selMunic)
+		    .keySet()) {
+		items.add(new Item(cantonCod, cantonNames.get(cantonCod)));
+	    }
+	}
+	cantonCombo.setModel(new DefaultComboBoxModel(items));
+	cantonCombo.setSelectedIndex(0);
+    }
+
+    private void updateAlternativaCombo() {
+	Vector<String> items = new Vector<String>();
+	String selDepart = ((Item) departCombo.getSelectedItem()).id;
+	String selMunic = ((Item) municCombo.getSelectedItem()).id;
+	String selCanton = ((Item) cantonCombo.getSelectedItem()).id;
+	if ((divsCodes.containsKey(selDepart))
+		&& (divsCodes.get(selDepart).containsKey(selMunic))
+		&& (divsCodes.get(selDepart).get(selMunic)
+			.containsKey(selCanton))) {
+	    for (String alternCod : divsCodes.get(selDepart).get(selMunic)
+		    .get(selCanton)) {
+		items.add(alternCod);
+	    }
+	}
+	alternCombo.setModel(new DefaultComboBoxModel(items));
+	if (items.size() > 0) {
+	    alternCombo.setSelectedIndex(0);
+	}
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	try {
+	    if (e.getSource() == okButton) {
+		String alternCod = alternCombo.getSelectedItem().toString();
+		OpenAlternativeExtension.openAlternative(alternCod);
+	    }
+	} catch (Exception e1) {
+	    e1.printStackTrace();
+	}
+	PluginServices.getMDIManager().closeWindow(this);
+
+    }
+
+    public Object getWindowProfile() {
+	return null;
+    }
+
+    private class Item {
+	private String id;
+	private String description;
+
+	public Item(String id, String description) {
+	    this.id = id;
+	    this.description = description;
+	}
+
+	public String toString() {
+	    return description;
+	}
     }
 
 }
