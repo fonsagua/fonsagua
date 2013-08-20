a1434205b2ef0b6a6e6636a68735e0c34138eec3
Jorge López <jlopez@cartolab.es>
2013-08-20 16:58:10 +0200

    Openalternative extension improved.
    Now it has some static and public methods, and it
    also cleans the view from the alternatives' layers
    before loading them again.

--- src/es/udc/cartolab/gvsig/fonsagua/OpenAlternativeExtension.java
+++ src/es/udc/cartolab/gvsig/fonsagua/OpenAlternativeExtension.java
@@ -1,9 +1,12 @@
 package es.udc.cartolab.gvsig.fonsagua;
 
+import java.util.Set;
+
 import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
 import com.iver.andami.ui.mdiManager.IWindow;
 import com.iver.cit.gvsig.ProjectExtension;
+import com.iver.cit.gvsig.fmap.layers.FLayers;
 import com.iver.cit.gvsig.project.Project;
 import com.iver.cit.gvsig.project.documents.ProjectDocument;
 import com.iver.cit.gvsig.project.documents.ProjectDocumentFactory;
@@ -13,40 +16,13 @@ import com.iver.cit.gvsig.project.documents.view.gui.View;
 import es.udc.cartolab.gvsig.elle.utils.ELLEMap;
 import es.udc.cartolab.gvsig.elle.utils.LoadLegend;
 import es.udc.cartolab.gvsig.elle.utils.MapDAO;
+import es.udc.cartolab.gvsig.fonsagua.forms.alternativas.AlternativasForm;
 import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaConstants;
+import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaFormFactory;
 import es.udc.cartolab.gvsig.fonsagua.utils.OpenAlternativeDialog;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 
 public class OpenAlternativeExtension extends Extension {
-
-    /**
-     * If the active window is a View returns it, if not creates a new one, adds
-     * it to the project and returns it
-     */
-    private View createViewIfNeeded() {
-
-	// TODO: fpuga: Check what happens when exists a view in the project but
-	// is not active
-	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
-	View view = null;
-
-	if (iWindow instanceof View) {
-	    view = (View) iWindow;
-	} else {
-
-	    Project project = ((ProjectExtension) PluginServices
-		    .getExtension(ProjectExtension.class)).getProject();
-	    ProjectDocumentFactory viewFactory = Project
-		    .getProjectDocumentFactory(ProjectViewFactory.registerName);
-	    ProjectDocument projectDocument = viewFactory.create(project);
-	    projectDocument.setName("Alternativa");
-	    project.addDocument(projectDocument);
-	    view = (View) projectDocument.createWindow();
-	    view.getWindowInfo().setMaximized(true);
-	    PluginServices.getMDIManager().addWindow(view);
-	}
-	return view;
-    }
 
     private static String code;
 
@@ -69,6 +45,10 @@ public class OpenAlternativeExtension extends Extension {
 	// TODO: What must be done if we already opened an alternative and the
 	// user press the button again.
 	// PluginServices.getMDIManager().closeAllWindows();
+	openAlternative(code);
+    }
+
+    public static void openAlternative(String code) {
 	MapDAO mapDAO = MapDAO.getInstance();
 	try {
 	    final View view = createViewIfNeeded();
@@ -77,8 +57,8 @@ public class OpenAlternativeExtension extends Extension {
 		    FonsaguaConstants.AlternativesMap);
 
 	    // TODO: This strings should not be hard coded
-	    final String sqlWhere = "where cod_alternativa = '"
-		    + OpenAlternativeExtension.code + "'";
+	    final String sqlWhere = "where " + AlternativasForm.PKFIELD
+		    + " = '" + code + "'";
 	    map.getLayer("alt_bombeos").setWhere(sqlWhere);
 	    map.getLayer("alt_conexiones").setWhere(sqlWhere);
 	    map.getLayer("alt_depositos").setWhere(sqlWhere);
@@ -91,6 +71,47 @@ public class OpenAlternativeExtension extends Extension {
 	} catch (Exception e) {
 	    // TODO: catch error
 	    e.printStackTrace();
+	}
+    }
+
+    /**
+     * If the active window is a View returns it, if not creates a new one, adds
+     * it to the project and returns it
+     */
+    private static View createViewIfNeeded() {
+
+	// TODO: fpuga: Check what happens when exists a view in the project but
+	// is not active
+	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+	View view = null;
+
+	if (iWindow instanceof View) {
+	    view = (View) iWindow;
+	    cleanViewIfNeeded(view);
+	} else {
+	    Project project = ((ProjectExtension) PluginServices
+		    .getExtension(ProjectExtension.class)).getProject();
+	    ProjectDocumentFactory viewFactory = Project
+		    .getProjectDocumentFactory(ProjectViewFactory.registerName);
+	    ProjectDocument projectDocument = viewFactory.create(project);
+	    projectDocument.setName("Alternativas");
+	    project.addDocument(projectDocument);
+	    view = (View) projectDocument.createWindow();
+	    view.getWindowInfo().setMaximized(true);
+	    PluginServices.getMDIManager().addWindow(view);
+	}
+	return view;
+    }
+
+    private static void cleanViewIfNeeded(View view) {
+	if (FonsaguaFormFactory.getInstance().allAlternativasLayersLoaded()) {
+	    Set<String> layers = FonsaguaFormFactory.getInstance()
+		    .getAllAlternativasLayersNames();
+	    FLayers layersGroup = view.getMapControl().getMapContext()
+		    .getLayers();
+	    for (String layer : layers) {
+		layersGroup.removeLayer(layer);
+	    }
 	}
     }
 
