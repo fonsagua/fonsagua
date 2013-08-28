7912c09c5e864d9a778ca60a86e994072de0ac4d
Francisco Puga <fpuga@cartolab.es>
2013-08-28 17:44:22 +0200

    Update form for 'fuentes' with the model changes

--- src/es/udc/cartolab/gvsig/fonsagua/forms/alternativas/AltFuentesForm.java
+++ src/es/udc/cartolab/gvsig/fonsagua/forms/alternativas/AltFuentesForm.java
@@ -8,6 +8,12 @@ import es.icarto.gvsig.navtableforms.BasicAbstractForm;
 public class AltFuentesForm extends BasicAbstractForm {
 
     public static final String NAME = "alt_fuentes";
+    public static String[] colNames = {
+"fuente", "tipo_fuente_alternativa", "existencia_elemento", "q_usar"
+    };
+    public static String[] colAlias = {
+"Nombre", "Tipo de fuente", "Estado", "Caudal a usar (l/s)"
+    };
     public AltFuentesForm(FLyrVect layer) {
 	super(layer);
     }
