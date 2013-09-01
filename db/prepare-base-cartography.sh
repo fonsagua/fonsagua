63d7b8328029cd3c4fc84b13227cb77163cb6632
Francisco Puga <fpuga@cartolab.es>
2013-09-01 18:48:28 +0200

    Scripts to handle db deployment

---
+++ db/prepare-base-cartography.sh
@@ -0,0 +1,28 @@
+# Flow to use with this script
+# 1) GIS technician prepares the base cartography (c_base schema) and
+#    the maps (elle schema) in a local db in his computer
+# 2) A backup of elle and c_base schema is done with pgadmin in custom format is done by the GIS technician.
+# 3) DevOp restores the dump in his own computer, reasigns the ownership to fonsagua user and cleans the not needed maps of elle
+
+
+createdb -h localhost -T template_postgis -U postgres --owner fonsagua fonsagua_c_base || exit -1
+
+psql -h localhost -U postgres -d fonsagua_c_base -c \
+    	"ALTER SCHEMA public OWNER TO fonsagua; \
+         ALTER TABLE public.geometry_columns OWNER TO fonsagua; \
+         ALTER TABLE public.spatial_ref_sys OWNER TO fonsagua; \
+         ALTER TABLE public.geography_columns OWNER TO fonsagua;"
+
+#pg_restore -U fonsagua -d fonsagua_c_base --no-owner /tmp/BDD_Fonsagua/fonsagua_elle.backup
+
+# Contains c_base and elle
+pg_restore -U fonsagua -d fonsagua_c_base --no-owner /tmp/BDD_Fonsagua/fonsagua_c_base.backup
+
+psql -h localhost -U fonsagua -d fonsagua_c_base -c "DELETE FROM elle._map WHERE mapa NOT IN ('Vista alternativas', 'Vista general');"
+psql -h localhost -U fonsagua -d fonsagua_c_base -c "DELETE FROM elle._map_overview WHERE mapa NOT IN ('Vista alternativas', 'Vista general');"
+psql -h localhost -U fonsagua -d fonsagua_c_base -c "DELETE FROM elle._map_style WHERE nombre_estilo NOT IN ('Vista alternativas', 'Vista general');"
+psql -h localhost -U fonsagua -d fonsagua_c_base -c "DELETE FROM elle._map_overview_style WHERE nombre_estilo NOT IN ('Vista alternativas', 'Vista general');"
+
+
+pg_dump -n c_base -U fonsagua --no-owner -x  -f /tmp/c_base.sql fonsagua_c_base
+pg_dump -n elle -U fonsagua --no-owner -x  -f /tmp/elle.sql fonsagua_c_base
--- db/prepare-db-deploy.sh
+++ db/prepare-db-deploy.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 error() {
@@ -5,27 +6,41 @@ error() {
     exit -1
 }
 
-CARTOGRAFIA_BASE=/home/fpuga/Escritorio/cartolab/Fonsagua/BDD_Fonsagua/
+CARTOGRAFIA_BASE=/tmp
+
 output=/tmp/`date +%Y%m%d`-fonsagua-bbdd.sql
 
 [ -d "$CARTOGRAFIA_BASE" ] || error "Falta el directorio de la cartografía base"
 
-echo "ALTER SCHEMA public OWNER TO fonsagua" > $output
-echo "ALTER TABLE public.geometry_columns OWNER TO fonsagua" >> $output
-echo "ALTER TABLE public.spatial_ref_sys OWNER TO fonsagua" >> $output
-echo "ALTER TABLE public.geography_columns OWNER TO fonsagua" >> $output
-cat ./data/01-create-data-schemas.sql  > $output
+echo "" > $output
+# echo "ALTER SCHEMA public OWNER TO fonsagua" >> $output
+# echo "ALTER TABLE public.geometry_columns OWNER TO fonsagua" >> $output
+# echo "ALTER TABLE public.spatial_ref_sys OWNER TO fonsagua" >> $output
+# echo "ALTER TABLE public.geography_columns OWNER TO fonsagua" >> $output
+cat $CARTOGRAFIA_BASE/elle.sql >> $output
+cat $CARTOGRAFIA_BASE/c_base.sql >> $output
+cat ./data/01-create-data-schemas.sql >> $output
 cat ./data/02-domains.sql >> $output
 cat ./data/03-comunidades.sql >> $output
 cat ./data/04-comunidades_croquis.sql >> $output
 cat ./data/05-relations.sql >> $output
 cat ./data/06-triggers.sql >> $output
+cat ./data/07-domains_alternatives.sql >> $output
+cat ./data/08-data_alternatives.sql >> $output
+# cat ./data/09-elle_alternatives.sql
+cat ./data/11-triggers-alt-fuentes.sql >> $output
+cat ./data/12-triggers-alt-conexiones.sql >> $output
+cat ./data/13-triggers-alt-tuberias.sql >> $output
+cat ./data/14-triggers-alt-depositos.sql >> $output
+cat ./data/15-triggers-alternativas.sql >> $output
+cat ./data/16-triggers-alt-bombas.sql >> $output
+cat ./data/17-preferencias_bombas.sql >> $output
+cat ./data/18-preferencias_tuberias.sql >> $output
 
-cat $CARTOGRAFIA_BASE/elle.sql >> $output
-cat $CARTOGRAFIA_BASE/c_base.sql >> $output
+
+
 
 echo "RESET search_path;" >> $output
 echo "select populate_geometry_columns();" >> $output
 
 echo "VACUUM ANALYZE;" >> $output
-
