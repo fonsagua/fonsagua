65bf7e71d732aa192e51a89413b6ddee6159fe3c
Francisco Puga <fpuga@cartolab.es>
2013-09-02 13:30:06 +0200

    Adds an InitializeExtension where the presets or general configs needed for the application are done

--- src/es/udc/cartolab/gvsig/fonsagua/FormsExtension.java
+++ src/es/udc/cartolab/gvsig/fonsagua/FormsExtension.java
@@ -1,33 +1,16 @@
 package es.udc.cartolab.gvsig.fonsagua;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.iver.andami.Launcher;
 import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
-import com.iver.andami.plugins.ExtensionDecorator;
-import com.iver.andami.plugins.IExtension;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
-import com.iver.cit.gvsig.gui.cad.tools.SelectionCADTool;
 import com.iver.utiles.extensionPoints.ExtensionPoints;
 import com.iver.utiles.extensionPoints.ExtensionPointsSingleton;
 
 import es.icarto.gvsig.navtableforms.AbstractForm;
 import es.icarto.gvsig.navtableforms.utils.FormFactory;
 import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
-import es.udc.cartolab.gvsig.elle.DeleteAllLegendsExtension;
-import es.udc.cartolab.gvsig.elle.DeleteMapExtension;
-import es.udc.cartolab.gvsig.elle.LoadAllLegendsExtension;
-import es.udc.cartolab.gvsig.elle.LoadMapExtension;
-import es.udc.cartolab.gvsig.elle.SaveAllLegendsExtension;
-import es.udc.cartolab.gvsig.elle.SaveMapExtension;
-import es.udc.cartolab.gvsig.fonsagua.config.EpanetConfiguration;
-import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaConstants;
 import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaFormFactory;
 import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaTocMenuEntry;
-import es.udc.cartolab.gvsig.tools.CopyFeaturesExtension;
-import es.udc.cartolab.gvsig.users.SpatiaLiteDBConnectionExtension;
 
 public class FormsExtension extends Extension {
 
@@ -64,42 +47,6 @@ public class FormsExtension extends Extension {
 		.getInstance();
 	extensionPoints.add("View_TocActions", "Fonsagua",
 		new FonsaguaTocMenuEntry());
-
-	// Workaround to use another icon for CopyFeatures extension instead of
-	// the default
-	PluginServices.getIconTheme().registerDefault(
-		CopyFeaturesExtension.COPY_FEATURES_ICON,
-		this.getClass().getClassLoader()
-			.getResource("images/copy_features.png"));
-	EpanetConfiguration epanetConfig = new EpanetConfiguration();
-	epanetConfig.setConfig();
-    }
-
-    @Override
-    public void postInitialize() {
-	// Workaround to increase the snap tolerance
-	SelectionCADTool.tolerance = 12;
-
-	// Workaround to open CopyFeatures dialog in a predefined folder
-	CopyFeaturesExtension.setDefaultPath(Launcher.getAppHomeDir()
-		+ FonsaguaConstants.GPS_MATCHING_FILES);
-
-	// Hide extensions that we only use as libraries and not as extensions
-	// to be "clicked" by the users
-	List<Class<? extends IExtension>> l = new ArrayList<Class<? extends IExtension>>();
-	l.add(SpatiaLiteDBConnectionExtension.class);
-	l.add(DeleteAllLegendsExtension.class);
-	l.add(DeleteMapExtension.class);
-	l.add(LoadAllLegendsExtension.class);
-	l.add(LoadMapExtension.class);
-	l.add(SaveAllLegendsExtension.class);
-	l.add(SaveMapExtension.class);
-
-	for (Class<? extends IExtension> c : l) {
-	    PluginServices.getDecoratedExtension(c).setVisibility(
-		    ExtensionDecorator.ALWAYS_INVISIBLE);
-	}
-
     }
 
     @Override
---
+++ src/es/udc/cartolab/gvsig/fonsagua/InitializeExtension.java
@@ -0,0 +1,85 @@
+package es.udc.cartolab.gvsig.fonsagua;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.iver.andami.Launcher;
+import com.iver.andami.PluginServices;
+import com.iver.andami.plugins.Extension;
+import com.iver.andami.plugins.ExtensionDecorator;
+import com.iver.andami.plugins.IExtension;
+import com.iver.cit.gvsig.gui.cad.tools.SelectionCADTool;
+
+import es.udc.cartolab.gvsig.fonsagua.config.EpanetConfiguration;
+import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaConstants;
+import es.udc.cartolab.gvsig.tools.CopyFeaturesExtension;
+import es.udc.cartolab.gvsig.users.PostGISDBConnectionExtension;
+import es.udc.cartolab.gvsig.users.SpatiaLiteDBConnectionExtension;
+
+public class InitializeExtension extends Extension {
+
+    @Override
+    public void initialize() {
+	// Workaround to use another icon for CopyFeatures extension instead of
+	// the default
+	PluginServices.getIconTheme().registerDefault(
+		CopyFeaturesExtension.COPY_FEATURES_ICON,
+		this.getClass().getClassLoader()
+			.getResource("images/copy_features.png"));
+	EpanetConfiguration epanetConfig = new EpanetConfiguration();
+	epanetConfig.setConfig();
+
+    }
+
+    @Override
+    public void postInitialize() {
+	// Workaround to increase the snap tolerance
+	SelectionCADTool.tolerance = 12;
+
+	// Workaround to open CopyFeatures dialog in a predefined folder
+	CopyFeaturesExtension.setDefaultPath(Launcher.getAppHomeDir()
+		+ FonsaguaConstants.GPS_MATCHING_FILES);
+
+	// Hide extensions that we only use as libraries and not as extensions
+	// to be "clicked" by the users
+	List<Class<? extends IExtension>> l = new ArrayList<Class<? extends IExtension>>();
+	l.add(SpatiaLiteDBConnectionExtension.class);
+	// l.add(DeleteAllLegendsExtension.class);
+	// l.add(DeleteMapExtension.class);
+	// l.add(LoadAllLegendsExtension.class);
+	// l.add(LoadMapExtension.class);
+	// l.add(SaveAllLegendsExtension.class);
+	// l.add(SaveMapExtension.class);
+
+	for (Class<? extends IExtension> c : l) {
+	    PluginServices.getDecoratedExtension(c).setVisibility(
+		    ExtensionDecorator.ALWAYS_INVISIBLE);
+	}
+
+	// Remove project manager
+	PluginServices.getMDIManager().closeAllWindows();
+
+	// Opens the connection dialog. The start up preferences configuration
+	// is not valid, because the closeAllWindows close this dialog also
+	PostGISDBConnectionExtension connectionDialog = (PostGISDBConnectionExtension) PluginServices
+		.getExtension(PostGISDBConnectionExtension.class);
+	connectionDialog.execute(null);
+
+    }
+
+    @Override
+    public void execute(String actionCommand) {
+	throw new AssertionError();
+    }
+
+    @Override
+    public boolean isEnabled() {
+	return false;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return false;
+    }
+
+}
