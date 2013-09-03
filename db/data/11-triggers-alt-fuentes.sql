b9b69e91508c7eacf2fd94211b3d9d6368441926
Francisco Puga <fpuga@cartolab.es>
2013-09-03 20:29:34 +0200

    coef_q_eco should only be applied for 'Punto rio' and 'Manantiales'

--- db/data/11-triggers-alt-fuentes.sql
+++ db/data/11-triggers-alt-fuentes.sql
@@ -3,10 +3,17 @@ CREATE OR REPLACE FUNCTION fonsagua.alt_fuentes_compute_field_trigger() RETURNS 
 	coef_q_eco_t REAL;
        BEGIN
 	SELECT COALESCE(coef_q_ecologico,0) INTO coef_q_eco_t FROM fonsagua.preferencias_disenho WHERE cod_alternativa = NEW.cod_alternativa;
+	IF NOT (NEW.aforo >= 0) THEN NEW.aforo = 0; END IF;
+	IF NOT (coef_q_eco_t >= 0) THEN coef_q_eco_t = 0; END IF;
 
-	IF (coef_q_eco_t > 0 AND NEW.aforo > 0) THEN NEW.q_ecologico = coef_q_eco_t * NEW.AFORO; ELSE NEW.q_ecologico = NULL; END IF;
 
-	IF (NEW.q_usar IS NULL OR NEW.q_usar > coef_q_eco_t  OR NEW.q_usar < 0) THEN NEW.q_usar = NEW.q_ecologico; END IF;
+	IF NEW.tipo_fuente_alternativa = 'Pozo' THEN
+	   NEW.q_ecologico = NULL;
+	   IF NEW.q_usar IS NULL THEN NEW.q_usar = 0; END IF;
+	ELSE
+	   NEW.q_ecologico = coef_q_eco_t * NEW.aforo;
+	   IF (NEW.q_usar IS NULL) OR (NEW.q_usar > NEW.q_ecologico) THEN NEW.q_usar = NEW.q_ecologico; END IF;
+	END IF;
 
 	NEW.q_calculo = -NEW.q_usar;
 
--- db/tests/triggers-alt_fuentes.sql
+++ db/tests/triggers-alt_fuentes.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT PLAN(9);
+SELECT PLAN(13);
 
 SELECT trigger_is('fonsagua', 'alt_fuentes', 'alt_fuentes_compute_field_trigger', 'fonsagua', 'alt_fuentes_compute_field_trigger');
 
@@ -8,7 +8,7 @@ INSERT INTO fonsagua.alternativas (cod_alternativa, departamento, municipio, can
 
 INSERT INTO fonsagua.preferencias_disenho (cod_alternativa, coef_q_ecologico) VALUES ('TEST-ALT-FUENTES', 0.5);
 
-INSERT INTO fonsagua.alt_fuentes(cod_alternativa, aforo, q_usar) VALUES ('TEST-ALT-FUENTES', 2, 0);
+INSERT INTO fonsagua.alt_fuentes(cod_alternativa, tipo_fuente_alternativa, aforo, q_usar) VALUES ('TEST-ALT-FUENTES', 'Manantial', 2, 0);
 SELECT is(q_ecologico, 1::numeric)
        FROM fonsagua.alt_fuentes
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
@@ -22,7 +22,12 @@ SELECT is(q_calculo, 0::numeric)
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
 
 UPDATE fonsagua.alt_fuentes SET aforo=-5 WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
-SELECT is(q_ecologico, NULL)
+
+SELECT is(aforo, 0::numeric)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_ecologico, 0::numeric)
        FROM fonsagua.alt_fuentes
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
 
@@ -30,15 +35,28 @@ SELECT is(q_usar, 0::numeric)
        FROM fonsagua.alt_fuentes
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
 
-INSERT INTO fonsagua.alt_fuentes(cod_alternativa, aforo, q_usar) VALUES ('TEST-ALT-FUENTES', 0.5, NULL);
+INSERT INTO fonsagua.alt_fuentes(cod_alternativa, aforo, q_usar, tipo_fuente_alternativa) VALUES ('TEST-ALT-FUENTES', 0.5, NULL, 'Manantial');
 SELECT is(q_ecologico, 0.25)
        FROM fonsagua.alt_fuentes
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
 
-SELECT is(q_usar, 0.25)
+SELECT is(q_usar, 0.25::numeric)
        FROM fonsagua.alt_fuentes
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
 
 SELECT is(q_calculo, -0.25)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+UPDATE fonsagua.alt_fuentes SET tipo_fuente_alternativa='Pozo', q_usar=10 WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+SELECT is(q_ecologico, NULL)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_usar, 10::numeric)
+       FROM fonsagua.alt_fuentes
+       WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
+
+SELECT is(q_calculo, -10::numeric)
        FROM fonsagua.alt_fuentes
        WHERE gid = currval('fonsagua.alt_fuentes_gid_seq');
