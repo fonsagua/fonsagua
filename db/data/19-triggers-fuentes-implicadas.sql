252436c814d2144931c36fc3296f6bc93bc3b60f
Francisco Puga <fpuga@cartolab.es>
2013-09-12 10:35:44 +0200

    Adds trigger for 'fuentes_implicadas'

---
+++ db/data/19-triggers-fuentes-implicadas.sql
@@ -0,0 +1,27 @@
+
+-- This trigger is used as a workaround to recalculate q_ecol and q_usar when the user changes the 
+-- preferences. From preferences table a "mock" uptade is done on the implicated tables to force to
+-- recalculate the triggers
+CREATE OR REPLACE FUNCTION fonsagua.fuentes_implicadas_compute_field_trigger() RETURNS TRIGGER AS $BODY$
+
+       DECLARE
+	coef_q_ecologico_t FLOAT;
+       BEGIN
+       SELECT coef_q_ecologico INTO coef_q_ecologico_t FROM preferencias_disenho WHERE cod_alternativa = NEW.cod_alternativa;
+
+	IF NEW.tipo_fuente IN ('Manantial', 'Punto rio') THEN
+	   NEW.q_ecol = coef_q_ecologico_t * NEW.aforo;
+	   IF NEW.q_usar > NEW.q_ecol THEN
+	      NEW.q_usar = 0;
+	   END IF;
+	END IF;
+	RETURN NEW;
+       END;
+$BODY$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS fuentes_implicadas_compute_field_trigger ON fonsagua.fuentes_implicadas;
+CREATE TRIGGER fuentes_implicadas_compute_field_trigger
+BEFORE UPDATE ON fonsagua.fuentes_implicadas
+FOR EACH ROW EXECUTE PROCEDURE fonsagua.fuentes_implicadas_compute_field_trigger();
+
+
---
+++ db/tests/triggers-fuentes_implicadas.sql
@@ -0,0 +1,30 @@
+BEGIN;
+
+SELECT PLAN(5);
+
+SELECT trigger_is('fonsagua', 'fuentes_implicadas', 'fuentes_implicadas_compute_field_trigger', 'fonsagua', 'fuentes_implicadas_compute_field_trigger');
+
+INSERT INTO fonsagua.alternativas (cod_alternativa, departamento, municipio, canton) VALUES ('TEST-FUENTES-IMPLICADAS','mun', 'dep', 'canton');
+
+INSERT INTO fonsagua.preferencias_disenho (cod_alternativa, coef_q_ecologico) VALUES ('TEST-FUENTES-IMPLICADAS', 0.2);
+
+INSERT INTO fonsagua.fuentes_implicadas VALUES (default, 'TEST-FUENTES-IMPLICADAS', 'fuente-manantial', 'Manantial', 10, 10, 10);
+
+SELECT is(q_ecol, 10::numeric)
+       FROM fonsagua.fuentes_implicadas
+       WHERE gid = currval('fonsagua.fuentes_implicadas_gid_seq');
+
+SELECT is(q_usar, 10::numeric)
+       FROM fonsagua.fuentes_implicadas
+       WHERE gid = currval('fonsagua.fuentes_implicadas_gid_seq');
+
+
+UPDATE fonsagua.fuentes_implicadas SET cod_alternativa = 'TEST-FUENTES-IMPLICADAS' WHERE cod_alternativa = 'TEST-FUENTES-IMPLICADAS';
+
+SELECT is(q_ecol, 2::numeric)
+       FROM fonsagua.fuentes_implicadas
+       WHERE gid = currval('fonsagua.fuentes_implicadas_gid_seq');
+
+SELECT is(q_usar, 0::numeric)
+       FROM fonsagua.fuentes_implicadas
+       WHERE gid = currval('fonsagua.fuentes_implicadas_gid_seq');
