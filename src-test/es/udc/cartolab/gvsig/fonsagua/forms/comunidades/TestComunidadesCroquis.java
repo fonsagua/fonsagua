dc85b6972f738530ca218f7a75c3647eb55b7ecd
Jorge López <jlopez@cartolab.es>
2013-10-03 17:43:14 +0200

    Croquis tests updated.
    They don't work with Postgres 9.x server yet...

--- src-test/es/udc/cartolab/gvsig/fonsagua/forms/comunidades/TestComunidadesCroquis.java
+++ src-test/es/udc/cartolab/gvsig/fonsagua/forms/comunidades/TestComunidadesCroquis.java
@@ -2,141 +2,142 @@ package es.udc.cartolab.gvsig.fonsagua.forms.comunidades;
 
 import static org.junit.Assert.assertTrue;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.Arrays;
 
-import org.junit.Before;
+import javax.imageio.ImageIO;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.iver.cit.gvsig.fmap.drivers.DBException;
 
+import es.icarto.gvsig.navtableforms.DataBaseProperties;
+import es.icarto.gvsig.navtableforms.Drivers;
+import es.icarto.gvsig.navtableforms.TestProperties;
 import es.udc.cartolab.gvsig.fonsagua.utils.FonsaguaConstants;
-import es.udc.cartolab.gvsig.fonsagua.utils.ImageUtils;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 import es.udc.cartolab.gvsig.users.utils.DBSessionPostGIS;
 
 public class TestComunidadesCroquis {
 
-    Connection connection;
-    DBSession session;
+    private static DBSession session;
 
-    @Before
-    public void doSetupCroquis() {
-	String url = "jdbc:postgresql://localhost:5432/fonsagua";
-	String user = "postgres";
-	String passwd = "postgres";
-	// postgresql-9.1-903.jdbc3.jar needs to be in the classpasth before th
-	// other gvSIG jars related to pgsql.
-	// Configure that in your classpath tab if you use eclipse
-	try {
-	    Class.forName("org.postgresql.Driver");
-	    session = DBSessionPostGIS.createConnectionFromConnString(url,
-		    user, passwd);
-	} catch (ClassNotFoundException e) {
-	    e.printStackTrace();
-	} catch (DBException e) {
-	    e.printStackTrace();
+    static boolean bufferedImageEquals(BufferedImage b1, BufferedImage b2) {
+	if (b1 == b2) {
+	    return true;
+	} // true if both are null
+	if (b1 == null || b2 == null) {
+	    return false;
 	}
-
-	String query = "INSERT INTO fonsagua.comunidades(cod_comunidad, cod_departamento,"
-		+ " cod_municipio, cod_canton, cod_caserio) VALUES ('1', '1', '1', '1', '1')";
-	PreparedStatement statement;
-	try {
-	    statement = connection.prepareStatement(query);
-	    statement.execute();
-	    connection.commit();
-	} catch (SQLException e) {
-	    try {
-		connection.close();
-		connection = DriverManager.getConnection(url, user, passwd);
-		connection.setAutoCommit(false);
-	    } catch (SQLException e1) {
-		e1.printStackTrace();
+	if (b1.getWidth() != b2.getWidth()) {
+	    return false;
+	}
+	if (b1.getHeight() != b2.getHeight()) {
+	    return false;
+	}
+	for (int i = 0; i < b1.getWidth(); i++) {
+	    for (int j = 0; j < b1.getHeight(); j++) {
+		if (b1.getRGB(i, j) != b2.getRGB(i, j)) {
+		    return false;
+		}
 	    }
 	}
+	return true;
+    }
+
+    @BeforeClass
+    public static void doSetupCroquis() throws DBException, SQLException {
+	Drivers.initgvSIGDrivers(TestProperties.driversPath);
+	DBSessionPostGIS.createConnection(DataBaseProperties.server,
+		DataBaseProperties.port, DataBaseProperties.dbname, null,
+		DataBaseProperties.user, DataBaseProperties.userpwd);
+
+	String[] columns = { "cod_comunidad", "cod_departamento",
+		"cod_municipio", "cod_canton", "cod_caserio" };
+	Object[] values = { 1, 1, 1, 1, 1 };
+	session = DBSession.getCurrentSession();
+	session.insertRow("fonsagua", "comunidades", columns, values);
+    }
+
+    @AfterClass
+    public static void doCloseCroquis() throws DBException, SQLException {
+	session.deleteRows("fonsagua", "comunidades",
+		" WHERE cod_comunidad = '1'");
+	session.close();
+    }
+
+    @After
+    public void deleteCroquis() throws DBException, SQLException {
+	session.deleteRows("fonsagua", "comunidades_croquis",
+		" WHERE cod_comunidad = '1'");
     }
 
     @Test
     public void testingInsertAndReadCroquis() throws Exception {
 
-	try {
-	    // TODO: Change by example croquis
-	    File image = new File("data-test/test.jpg");
-	    String query = "DELETE FROM fonsagua.comunidades_croquis";
-	    PreparedStatement statement = connection.prepareStatement(query);
-	    statement.execute();
-	    connection.commit();
+	// TODO: Change by example croquis
+	File image = new File("data-test/test.jpg");
+	session.deleteRows("fonsagua", "comunidades_croquis",
+		" WHERE cod_comunidad = '1'");
 
-	    byte[] imageBytes = ImageUtils.convertImageToBytea(image);
-	    Object[] values = { "1", imageBytes };
-	    String[] columns = {
-		    FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME,
-		    FonsaguaConstants.CROQUIS_FIELDNAME };
-	    session.insertRow(FonsaguaConstants.dataSchema,
-		    FonsaguaConstants.CROQUIS_TABLENAME, columns, values);
+	InputStream is1 = new FileInputStream(image);
+	Object[] values = { "1" };
+	String[] columns = { FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME };
+	session.insertWithBinaryStream(FonsaguaConstants.CROQUIS_TABLENAME,
+		FonsaguaConstants.dataSchema,
+		    FonsaguaConstants.CROQUIS_FIELDNAME, is1, (int) image.length(), columns,
+		    values);
 
-	    String[] columns3 = { FonsaguaConstants.CROQUIS_FIELDNAME };
-	    Object[][] values3 = session.getTableAsObjects(
-		    FonsaguaConstants.CROQUIS_TABLENAME,
-		    FonsaguaConstants.dataSchema, columns3, "WHERE "
+	InputStream is2 = session.getBinaryStream(
+		FonsaguaConstants.CROQUIS_TABLENAME,
+		FonsaguaConstants.dataSchema,
+		    FonsaguaConstants.CROQUIS_FIELDNAME, "WHERE "
 			    + FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME
-			    + " = '1'", new String[0], false);
-	    byte[] imageDbBytes = (byte[]) values3[0][0];
+			    + " = '1'");
 
-	    byte[] imageMockBytes = ImageUtils.convertImageToBytea(image);
-
-	    assertTrue(Arrays.equals(imageDbBytes, imageMockBytes));
-	} finally {
-	    connection.rollback();
-	}
+	BufferedImage image1 = ImageIO.read(new FileInputStream(image));
+	BufferedImage image2 = ImageIO.read(is2);
+	assertTrue(bufferedImageEquals(image1, image2));
     }
 
     @Test
     public void testingUpdateAndReadCroquis() throws Exception {
 
-	try {
-	    // TODO: Change by example croquis
-	    File image = new File("data-test/test.jpg");
-	    String query = "DELETE FROM fonsagua.comunidades_croquis";
-	    PreparedStatement statement = connection.prepareStatement(query);
-	    statement.execute();
-	    connection.commit();
+	// TODO: Change by example croquis
+	File image = new File("data-test/test.jpg");
 
-	    byte[] imageBytes = ImageUtils.convertImageToBytea(image);
-	    Object[] values = { "1", imageBytes };
-	    String[] columns = {
-		    FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME,
-		    FonsaguaConstants.CROQUIS_FIELDNAME };
-	    session.insertRow(FonsaguaConstants.dataSchema,
-		    FonsaguaConstants.CROQUIS_TABLENAME, columns, values);
-	    File imageToUpdate = new File("data-test/test2.jpg");
-	    imageBytes = ImageUtils.convertImageToBytea(imageToUpdate);
-	    Object[] values2 = { imageBytes };
-	    String[] columns2 = { FonsaguaConstants.CROQUIS_FIELDNAME };
-	    session.updateRows(FonsaguaConstants.dataSchema,
-		    FonsaguaConstants.CROQUIS_TABLENAME, columns2, values2,
+	InputStream is1 = new FileInputStream(image);
+	Object[] values = { "1" };
+	String[] columns = { FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME };
+	session.insertWithBinaryStream(FonsaguaConstants.CROQUIS_TABLENAME,
+		FonsaguaConstants.dataSchema,
+		    FonsaguaConstants.CROQUIS_FIELDNAME, is1, (int) image.length(), columns,
+		    values);
+	File imageToUpdate = new File("data-test/test2.jpg");
+	is1 = new FileInputStream(imageToUpdate);
+	session.updateWithBinaryStream(FonsaguaConstants.CROQUIS_TABLENAME,
+		FonsaguaConstants.dataSchema,
+		    FonsaguaConstants.CROQUIS_FIELDNAME, is1,
+		    (int) image.length(), new String[0], new Object[0],
 		    "WHERE " + FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME
 			    + " = '1'");
 
-	    String[] columns3 = { FonsaguaConstants.CROQUIS_FIELDNAME };
-	    Object[][] values3 = session.getTableAsObjects(
-		    FonsaguaConstants.CROQUIS_TABLENAME,
-		    FonsaguaConstants.dataSchema, columns3, "WHERE "
+	InputStream is2 = session.getBinaryStream(
+		FonsaguaConstants.CROQUIS_TABLENAME,
+		    FonsaguaConstants.dataSchema,
+		    FonsaguaConstants.CROQUIS_FIELDNAME, "WHERE "
 			    + FonsaguaConstants.CROQUIS_COMUNIDAD_FK_FIELDNAME
-			    + " = '1'", new String[0], false);
-	    byte[] imageDbBytes = (byte[]) values3[0][0];
+			    + " = '1'");
 
-	    byte[] imageMockBytes = ImageUtils
-		    .convertImageToBytea(imageToUpdate);
-
-	    assertTrue(Arrays.equals(imageDbBytes, imageMockBytes));
-	} finally {
-	    connection.rollback();
-	}
+	BufferedImage image1 = ImageIO.read(new FileInputStream(imageToUpdate));
+	BufferedImage image2 = ImageIO.read(is2);
+	assertTrue(bufferedImageEquals(image1, image2));
     }
 
 }
