bed0696fde4636d65e6a37d2b4dd4de29f775bf2
Jorge López <jlopez@cartolab.es>
2014-01-17 21:20:42 +0100

    Removed some wrong trigger lines.

--- db/data_sqlite/10-triggers-alt-fuentes.sql
+++ db/data_sqlite/10-triggers-alt-fuentes.sql
@@ -6,7 +6,6 @@ FOR EACH ROW BEGIN
 	UPDATE alt_fuentes SET q_ecologico = NULL WHERE tipo_fuente_alternativa = 'Pozo' AND gid = NEW.gid;
 	UPDATE alt_fuentes SET q_usar = 0 WHERE tipo_fuente_alternativa = 'Pozo' AND gid = NEW.gid AND q_usar IS NULL;
 	UPDATE alt_fuentes SET q_ecologico = (SELECT IFNULL(coef_q_ecologico,0) FROM preferencias_disenho WHERE cod_alternativa = alt_fuentes.cod_alternativa) * aforo WHERE tipo_fuente_alternativa != 'Pozo' AND gid = NEW.gid AND (SELECT IFNULL(coef_q_ecologico,0) FROM preferencias_disenho WHERE cod_alternativa = alt_fuentes.cod_alternativa) >= 0;
-	UPDATE alt_fuentes SET q_ecologico = 0 WHERE tipo_fuente_alternativa != 'Pozo' AND gid = NEW.gid AND (SELECT IFNULL(coef_q_ecologico,0) FROM preferencias_disenho WHERE cod_alternativa = alt_fuentes.cod_alternativa) >= 0;
 	UPDATE alt_fuentes SET q_usar = q_ecologico WHERE tipo_fuente_alternativa != 'Pozo' AND gid = NEW.gid AND (q_usar IS NULL OR q_usar > q_ecologico);
 	UPDATE alt_fuentes SET q_calculo = -q_usar WHERE gid = NEW.gid;
 END;
@@ -19,7 +18,6 @@ FOR EACH ROW BEGIN
 	UPDATE alt_fuentes SET q_ecologico = NULL WHERE tipo_fuente_alternativa = 'Pozo' AND gid = NEW.gid;
 	UPDATE alt_fuentes SET q_usar = 0 WHERE tipo_fuente_alternativa = 'Pozo' AND gid = NEW.gid AND q_usar IS NULL;
 	UPDATE alt_fuentes SET q_ecologico = (SELECT IFNULL(coef_q_ecologico,0) FROM preferencias_disenho WHERE cod_alternativa = alt_fuentes.cod_alternativa) * aforo WHERE tipo_fuente_alternativa != 'Pozo' AND gid = NEW.gid AND (SELECT IFNULL(coef_q_ecologico,0) FROM preferencias_disenho WHERE cod_alternativa = alt_fuentes.cod_alternativa) >= 0;
-	UPDATE alt_fuentes SET q_ecologico = 0 WHERE tipo_fuente_alternativa != 'Pozo' AND gid = NEW.gid AND (SELECT IFNULL(coef_q_ecologico,0) FROM preferencias_disenho WHERE cod_alternativa = alt_fuentes.cod_alternativa) >= 0;
 	UPDATE alt_fuentes SET q_usar = q_ecologico WHERE tipo_fuente_alternativa != 'Pozo' AND gid = NEW.gid AND (q_usar IS NULL OR q_usar > q_ecologico);
 	UPDATE alt_fuentes SET q_calculo = -q_usar WHERE gid = NEW.gid;
 END;
