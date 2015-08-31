d391200ed000461f5357457d5661d8d255facc10
Jorge López Fernández <jlopez@cartolab.es>
2015-08-31 14:04:40 +0200

    Added sorter button based on i18n support

--- src/es/icarto/gvsig/navtableforms/BasicAbstractForm.java
+++ src/es/icarto/gvsig/navtableforms/BasicAbstractForm.java
@@ -1,22 +1,44 @@
 package es.icarto.gvsig.navtableforms;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.RowSorter.SortKey;
+
+import org.apache.log4j.Logger;
+
+import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
 import com.iver.andami.PluginServices;
 import com.iver.andami.ui.mdiManager.WindowInfo;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
 import com.jeta.forms.components.panel.FormPanel;
 import com.jeta.forms.gui.common.FormException;
 
+import es.icarto.gvsig.commons.gui.OkCancelPanel;
+import es.icarto.gvsig.commons.utils.Field;
+import es.icarto.gvsig.navtableforms.gui.i18n.I18nResourceManager;
 import es.icarto.gvsig.navtableforms.gui.i18n.resource.I18nResource;
 import es.icarto.gvsig.navtableforms.gui.i18n.resource.JavaBundleI18nResource;
+import es.udc.cartolab.gvsig.fonsagua.AlternativeBudgetReportExtension;
+import es.udc.cartolab.gvsig.fonsagua.utils.Utils;
+import es.udc.cartolab.gvsig.navtable.contextualmenu.ChooseSortFieldDialog;
 
 @SuppressWarnings("serial")
 public abstract class BasicAbstractForm extends AbstractForm {
 
+    private static final Logger logger = Logger
+	    .getLogger(AlternativeBudgetReportExtension.class);
+
     public BasicAbstractForm(FLyrVect layer) {
 	super(layer);
+	addSorterButton();
 	deleteMessageKey = "confirm_delete_register_cascade";
     }
 
@@ -60,6 +82,50 @@ public abstract class BasicAbstractForm extends AbstractForm {
     	    windowInfo.setTitle(PluginServices.getText(this, getBasicName()));
 	}
 	return windowInfo;
+    }
+
+    protected void addSorterButton() {
+	java.net.URL imgURL = getClass().getClassLoader().getResource(
+		"sort.png");
+	JButton jButton = new JButton(new ImageIcon(imgURL));
+	jButton.setToolTipText("Ordenar registros");
+
+	jButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		List<String> columns = Utils.getFields(getSchema(), getBasicName());
+		List<Field> fields = new ArrayList<Field>();
+
+		I18nResourceManager translator = getI18nHandler().getResourceManager();
+		for (String column : columns) {
+		    fields.add(new Field(column, translator.getString(column)));
+		}
+
+		ChooseSortFieldDialog dialog = new ChooseSortFieldDialog(fields);
+
+		if (dialog.open().equals(OkCancelPanel.OK_ACTION_COMMAND)) {
+		    List<Field> sortedFields = dialog.getFields();
+		    List<SortKey> sortKeys = new ArrayList<SortKey>();
+		    SelectableDataSource sds = getRecordset();
+		    for (Field field : sortedFields) {
+			try {
+			    int fieldIdx = sds.getFieldIndexByName(field
+				    .getKey());
+			    sortKeys.add(new SortKey(fieldIdx, field
+				    .getSortOrder()));
+			} catch (ReadDriverException e1) {
+			    logger.error(e1.getStackTrace(), e1);
+			}
+		    }
+		    setSortKeys(sortKeys);
+		}
+	    }
+	});
+	getActionsToolBar().add(jButton);
+    }
+
+    protected String getSchema() {
+	return "public";
     }
     
     protected abstract String getBasicName();
