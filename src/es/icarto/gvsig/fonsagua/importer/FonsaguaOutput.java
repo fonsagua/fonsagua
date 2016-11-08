abe143e3276fa3a2943fd840beec44067a8c0ae6
Francisco Puga <fpuga@icarto.es>
2016-11-08 20:56:24 +0100

    Changes the alg to reorder the table in the importer

--- src/es/icarto/gvsig/fonsagua/importer/FonsaguaOutput.java
+++ src/es/icarto/gvsig/fonsagua/importer/FonsaguaOutput.java
@@ -87,24 +87,111 @@ public class FonsaguaOutput implements Output {
 	}
     }
 
-    protected void reorder(ImporterTM table) {
-	int lastComunidadesRow = -1;
-	for (int i = 0; i < table.getRowCount(); i++) {
-	    String tablename = table.getTarget(i).toString();
-	    if (tablename.equals("comunidades")
-		    && (lastComunidadesRow + 1) != i) {
-		lastComunidadesRow += 1;
-		table.moveRow(i, i, lastComunidadesRow);
+    public void reorder(ImporterTM table) {
+	putComunidadesFirst(table);
+	sortByCode(table, "comunidades");
+	putInformacionGeneralSecond(table);
+	sortByCode(table, "informacion_general");
+	sortOthersByCode(table);
+    }
+
+    private void putComunidadesFirst(ImporterTM table) {
+	for (int j = 0; j < table.getRowCount() - 1; j++) {
+	    for (int i = 0; i < table.getRowCount() - 1; i++) {
+		String tablename = table.getTarget(i).toString();
+		if (!tablename.equals("comunidades")) {
+		    table.moveRow(i, i, i + 1);
+		}
 	    }
 	}
+    }
 
-	int lastViviendaRow = lastComunidadesRow + 1;
-	for (int i = lastViviendaRow; i < table.getRowCount(); i++) {
+    private void putInformacionGeneralSecond(ImporterTM table) {
+	int first = -1;
+	for (int i = 0; i < table.getRowCount() - 1; i++) {
 	    String tablename = table.getTarget(i).toString();
-	    if (tablename.equals("informacion_general")
-		    && (lastViviendaRow + 1) != i) {
-		lastViviendaRow += 1;
-		table.moveRow(i, i, lastViviendaRow);
+	    if (!tablename.equals("comunidades")) {
+		first = i;
+		break;
+	    }
+	}
+	if (first == -1) {
+	    return;
+	}
+	for (int j = 0; j < table.getRowCount() - 1; j++) {
+	    for (int i = first; i < table.getRowCount() - 1; i++) {
+		String tablename = table.getTarget(i).toString();
+		if (!tablename.equals("informacion_general")) {
+		    table.moveRow(i, i, i + 1);
+		}
+	    }
+	}
+    }
+
+    private void sortByCode(ImporterTM table, String key) {
+	int length = table.getRowCount();
+	int first = -1;
+	for (int i = 0; i < length; i++) {
+	    String tablename = table.getTarget(i).toString();
+	    if (tablename.equals(key)) {
+		first = i;
+		break;
+	    }
+	}
+	if ((first != -1) && (first != length)) {
+	    boolean moved = true;
+	    while (moved) {
+		moved = false;
+		for (int i = first; i < length - 1; i++) {
+		    String tablename1 = table.getTarget(i).toString();
+		    String tablename2 = table.getTarget(i + 1).toString();
+		    if (!tablename1.equals(key)) {
+			break;
+		    }
+		    if (tablename2.equals(key)) {
+			String code1 = table.getCode(i);
+			String code2 = table.getCode(i + 1);
+			// System.out.println(String.format(
+			// "code1: %s, code2: %s", code1, code2));
+			if (code1.compareTo(code2) > 0) {
+			    moved = true;
+			    // System.out.println(String.format(
+			    // "row1: %s, row2: %s", i, i + 1));
+			    table.moveRow(i, i, i + 1);
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+    private void sortOthersByCode(ImporterTM table) {
+	int length = table.getRowCount();
+	int first = -1;
+	for (int i = 0; i < length; i++) {
+	    String tablename = table.getTarget(i).toString();
+	    if (!tablename.equals("comunidades")
+		    && !tablename.equals("informacion_general")) {
+		first = i;
+		break;
+	    }
+	}
+	if ((first != -1) && (first != length)) {
+	    boolean moved = true;
+	    while (moved) {
+		moved = false;
+		for (int i = first; i < length - 1; i++) {
+		    String code1 = table.getCode(i);
+		    String code2 = table.getCode(i + 1);
+		    // System.out.println(String.format("code1: %s, code2: %s",
+		    // code1, code2));
+		    if (code1.compareTo(code2) > 0) {
+			moved = true;
+			// System.out.println(String.format("row1: %s, row2: %s",
+			// i, i + 1));
+			table.moveRow(i, i, i + 1);
+		    }
+		}
 	    }
 	}
     }
